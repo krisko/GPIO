75cd08a661ab0a235cfab1e86944ab4acb95e62d
Krisko <krisko210@gmail.com>
2013-12-15 13:38:13 +0100

    snake reversed

--- 01-leds/01-leds.py
+++ 01-leds/01-leds.py
@@ -22,34 +22,33 @@ def LOFF(LED):
     GPIO.output(LED, False)
     return
 
-def LOFFall():
+def LOFFall(LEDS):
     for LED in LEDS:
         LOFF(LED)
     return
 
-# # # # #
-# BEGIN
-# # # # #
+def LEDinit():
+    GPIO.setmode(GPIO.BCM)
 
-GPIO.setmode(GPIO.BCM)
+    #define GPIO ports
+    LEDr1 = 22
+    LEDr2 = 17
+    LEDr3 = 18
+    LEDo1 = 23
+    LEDo2 = 24
+    LEDy1 = 25
+    LEDy2 = 10
+    LEDg1 = 4
+    LEDg2 = 9
+    LEDg3 = 11
 
-#define GPIO ports
-LEDr1 = 22
-LEDr2 = 17
-LEDr3 = 18
-LEDo1 = 23
-LEDo2 = 24
-LEDy1 = 25
-LEDy2 = 10
-LEDg1 = 4
-LEDg2 = 9
-LEDg3 = 11
+    #add led to list
+    LEDS= [ LEDr1, LEDr2, LEDr3, LEDo1, LEDo2, LEDy1, LEDy2, LEDg1, LEDg2, LEDg3 ]
 
-#add led to list
-LEDS= [ LEDr1, LEDr2, LEDr3, LEDo1, LEDo2, LEDy1, LEDy2, LEDg1, LEDg2, LEDg3 ]
+    for LED in LEDS:
+        GPIO.setup(LED, GPIO.OUT)
 
-for LED in LEDS:
-    GPIO.setup(LED, GPIO.OUT)
+    return LEDS
  
 def usage():
     print """Raspberry PI GPIO LED SCRIPT
@@ -57,38 +56,57 @@ KrisKo 2013
 
 OPTIONS:
     -i NR                   number of iterations
-    -s NR, --snake=NR       NR=time of LED shift
+    -s NR, --snake=NR       snake; NR=time of LED shift
+    -r NR, --snaker=NR      snake reversed; NR=time of LED shift
     """
-
-def snake(sec, repeat):
+#create snake like effect
+def snake(sec, repeat, LEDS):
     while (repeat > 0):
         for LED in LEDS:
             LEDact=LED
             LON(LEDact)
             try:
+                #if we have same LEDs, skip this iteration (this is for reversed LED list)
+                if LEDprev == LEDact:
+                    print "SAME LEDS (GPIO: %s), skipping iteration" % LEDact
+                    continue
                 LOFF(LEDprev)
             except:
                 pass
             time.sleep(sec)
             LEDprev=LEDact
-        repeat = repeat - 1
-    LOFFall()
+        repeat -= 1
+    LOFFall(LEDS)
+
+def snaker(sec, repeat, LEDS):
+    #remove first and last element from list
+    #LEDSr.pop(0)
+    #LEDSr.pop()
+    
+    #Join reversed list to LEDS
+    LEDSr = LEDS + LEDS[::-1]
+    #call snake with joined LEDS + reversed LEDS list
+    snake(sec, repeat, LEDSr)
+
 
 import getopt, sys
 
 def main():
     repeat = 1
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hi:s:", ["help", "snake="])
+        opts, args = getopt.getopt(sys.argv[1:], "hi:s:r:", ["help", "snake=", "snaker="])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err) # will print something like "option -a not recognized"
         sys.exit(2)
     for opt, arg in opts:
+        #iteration count
         if opt == "-i":
             repeat = arg
         elif opt in ("-s", "--snake"):
-            snake(float(arg), int(repeat))
+            snake(float(arg), int(repeat), LEDinit())
+        elif opt in ("-r", "--snaker"):
+            snaker(float(arg), int(repeat), LEDinit())
         elif opt in ("-h", "--help"):
             usage()
             sys.exit()
