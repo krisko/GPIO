e52fd2f39f0dbd8abc36dfd2588d269b25cdb5a3
Krisko <krisko210@gmail.com>
2014-06-23 19:50:18 +0200

    added single arg, comments...

--- 03-leds/03-leds.sh
+++ 03-leds/03-leds.sh
@@ -26,6 +26,7 @@ USAGE:
 EXAMPLE:
 	$0 help        display this help
 	$0             use STDIN
+	$0 <NR>        toggle NR LED (NR: 0-9)
 	$0 0.2 12345   blink leds 1..5, interval 0.2 sec
 	$0 inf 0.3 123 blink leds 1..3, interval 0.3 sec, infite loop
 
@@ -39,6 +40,7 @@ EOF
 cleanup(){
 	echo -e "\nINFO: Cleaning up..."
 	for LED in ${LEDArray[*]}; do
+        # only shutdown if the led was/is initialized
 		[ -L /sys/class/gpio/gpio$LED ] && echo 0 > /sys/class/gpio/gpio$LED/value
 	done
 	exit 0
@@ -59,15 +61,20 @@ toggle(){
 	echo $val > /sys/class/gpio/gpio$1/value 
 }
 
-# close STDERR so we wont see error msg from -le condition
+# close STDERR so we wont see error msg from invalid conditions
 exec 2>&-
 
+# do the core cycle
 iterate(){
 	PARAM=$1
+	# if true do only one led toggle and exit w/o cleanup (led stays as is was set)
+	NOCLEAN=$2
 	[ "$PARAM" == "inf" ] && INF=: && continue
-	[ -z $TIME ] && TIME=$PARAM && continue
+	[ ! $NOCLEAN ] && [ -z $TIME ] && TIME=$PARAM && continue
+	# defaul RUN is true
 	RUN=:
 	while $RUN; do
+		# do the iteration x-times
 		for (( c=0; c<${#PARAM}; c++ )); do
 			NUM=${PARAM:$c:1}
 			[ $NUM -le 9 ] && {
@@ -76,16 +83,23 @@ iterate(){
 			} || echo -e "\nERROR: invalid number"
 		sleep $TIME
 		done
+		# if not infinite cycle, set RUN to false (this will end while cycle)
 		[ ! $INF ] && RUN="false"
 	done
-	cleanup
+	# if NOCLEAN is true, don't clean, just exit
+	[ ! $NOCLEAN ] && cleanup || exit 0
 }
 
+# handle parameters specified from cmd line
 for PARAM; do
+	# call usage if we need help
 	[ "$1" == "help" -o "$1" == "--help" ] && usage
-	iterate $PARAM
+	# set only if one NR is specified from cmd line
+	[ "$#" -eq 1 ] && [ "$1" -le 9 ] && NOCLEAN=:
+	iterate $PARAM $NOCLEAN
 done
 
+# if no input was specified, use stdin
 echo "INFO: Enter number 0-9 to toggle LEDs state"
 while read -n1 NUM; do
 	[ $NUM -le 9 ] && {
